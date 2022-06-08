7cf2bdc5f278f56b500f17c8c4739ee908bb50ad
PolinaVvv <zipseal@yandex.ru>
2022-06-08 10:56:50 +0300

    all

---
+++ analyzer/__init__.py
@@ -0,0 +1,18 @@
+"""
+Сервис с REST API, анализирующий рынок для промо-акций.
+"""
+__author__ = 'Vostrikova Polina'
+__maintainer__ = __author__
+
+__email__ = 'zipseal@yandex.ru'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+
+__all__ = (
+    '__author__',
+    '__email__',
+    '__license__',
+    '__maintainer__',
+    '__version__',
+)
